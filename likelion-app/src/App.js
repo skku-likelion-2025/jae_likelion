b4e6712e89fb4cd5a72692695bf907f75aa96f1f
marcus159260 <marcus.yeo.2022@scis.smu.edu.sg>
2025-04-11 17:44:59 +0900

    new

--- likelion-app/src/App.js
+++ likelion-app/src/App.js
@@ -1,9 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Nav(props) {
+  const lis = [
+    <li><a href="/read/1">html</a></li>,
+    <li><a href="/read/2">css</a></li>,
+    <li><a href="/read/3">js</a></li>
+  ]
+  for(let i=0; i<props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/' + t.id}>{t.title}</a></li>)
+  }
   return (
-    <p>world</p>
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>Welcome to React</h2>
+      <h3>{props.id}</h3>
+    </article>
+  );
+
+}
+function Header(props) {
+  console.log('props', props, props.title, props.love)
+  return (
+    <header>
+      <h1>
+        <a href="">
+          WEB {props.love}
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function App() {
+  const topics = [
+    { id: 1, title: "html" },
+    { id: 2, title: "css" },
+    { id: 3, title: "javascript" }
+  ]
+  return (
+    <div>
+      <Header title="titleinside" love="loveinside" onChangeMode ={function(){
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics}></Nav>
+      <Article ></Article>
+    </div>
   );
 }
 
